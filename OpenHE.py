b2098e55c70abfc7446cff27ba9c6bd91428354c
Ryan Piao <yirsung@gmail.com>
2022-03-15 23:07:52 -0400

    Add files via upload

---
+++ OpenHE.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+from windowcapture import WindowCapture
+
+# Change the working directory to the folder this script is in.
+# Doing this because I'll be putting the files from each video in their own folder on GitHub
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+## Get windowCapture Name
+# WindowCapture.list_window_names()
+# exit()
+
+# WindowCapture Loop
+loop_time = time()
+while(True):
+
+    # get an updated image of the game
+    screenshot = WindowCapture().get_screenshot()
+
+    cv.imshow('Computer Vision', screenshot)
+
+    # debug the loop rate
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print('Done.')
